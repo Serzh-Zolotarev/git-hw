aa6b111d01699d93455af25f0c6f13683c74fbcb
Serzh-Zolotarev <serzh.zolotarev2014-2015@yandex.ru>
2025-03-28 19:56:33 +0300

    поправлен текст

--- numbers.go
+++ numbers.go
@@ -16,6 +16,6 @@ func main() {
 	if _, err := strconv.Atoi(n); err == nil {
 		fmt.Printf("Вы ввели число: %s\n", n)
 	} else {
-		fmt.Printf("Вы ввели: %s\n", n)
+		fmt.Printf("Вы ввели текст: %s\n", n)
 	}
 }
